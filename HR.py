8f048db51481412c2abac46541f6dee26a397c1e
Achraf-Trabelsi <achref.trabelsi@insat.ucar.tn>
2022-02-03 20:01:45 +0100

    second commit

--- HR.py
+++ HR.py
@@ -1,20 +1,26 @@
 
+from numpy import linspace
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import MultinomialNB
-
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.metrics import accuracy_score
+#Understanding the dataset
 data=pd.read_csv("Human_Resources_Employee_Attrition.csv")
 print(data.shape)
 print(data.columns)
 #print(data.isnull().sum())
 print(data.describe())
-
+#Matrice de correlation
 corrMatrix = data.corr()
 sns.heatmap(corrMatrix, annot=True)
 plt.show()
-
+x=linspace(1,14999,14999)
+plt.plot(x,data.left)
+plt.show()
+#creation de un train/test dataset
 label=data.left
 #print(label)
 dt=data
@@ -22,6 +28,11 @@ dt=dt.drop(labels='left',axis=1)
 #print(dt.columns)
 d_train,d_test,l_train,l_test=train_test_split(dt,label,test_size=0.33)
 #print(d_test,d_train,l_train,l_test)
-model=MultinomialNB()
-model.fit(d_train,l_train)
-model.predict(d_test[:])
+enc=OrdinalEncoder()
+d_train=enc.fit_transform(d_train)
+d_test=enc.fit_transform(d_test)
+clf=RandomForestClassifier(max_depth=3,random_state=0)
+print(clf)
+clf.fit(d_train,l_train)
+print(clf.predict(d_test))
+print(accuracy_score(l_test,clf.predict(d_test)))
