1fb7ab80492944803c515e4783ea33da5c6c75e2
Achraf-Trabelsi <achref.trabelsi@insat.ucar.tn>
2022-02-04 21:42:43 +0100

    third commit

--- HR.py
+++ HR.py
@@ -7,32 +7,46 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.metrics import accuracy_score
+
 #Understanding the dataset
+
 data=pd.read_csv("Human_Resources_Employee_Attrition.csv")
 print(data.shape)
 print(data.columns)
 #print(data.isnull().sum())
 print(data.describe())
-#Matrice de correlation
+
+#Data visualization
+
 corrMatrix = data.corr()
 sns.heatmap(corrMatrix, annot=True)
 plt.show()
-x=linspace(1,14999,14999)
-plt.plot(x,data.left)
+plt.bar(data.department,data.left)
+plt.xlabel("satisfaction_level")
+plt.ylabel("Left")
 plt.show()
+
 #creation de un train/test dataset
+
 label=data.left
 #print(label)
 dt=data
 dt=dt.drop(labels='left',axis=1)
 #print(dt.columns)
 d_train,d_test,l_train,l_test=train_test_split(dt,label,test_size=0.33)
-#print(d_test,d_train,l_train,l_test)
+
+
+#data preprocessiong
+
 enc=OrdinalEncoder()
 d_train=enc.fit_transform(d_train)
 d_test=enc.fit_transform(d_test)
+
+#model selection
+
 clf=RandomForestClassifier(max_depth=3,random_state=0)
-print(clf)
 clf.fit(d_train,l_train)
 print(clf.predict(d_test))
-print(accuracy_score(l_test,clf.predict(d_test)))
+
+#accurracy_score
+print("accuracy is : ",accuracy_score(l_test,clf.predict(d_test)))
