4a47641232a64c95485424a59c261a0ce54a41ba
Achraf-Trabelsi <achref.trabelsi@insat.ucar.tn>
2022-02-05 16:19:50 +0100

    fouth commit

--- app.py
+++ app.py
@@ -1,11 +1,49 @@
-import flask as f
- 
-app = f.Flask(__name__, template_folder='templates')
+import flask as fa
+import pickle 
+import pandas as pd
+
+with open(f'HRattrition_model_RandomForest.pkl', 'rb') as f:
+    model = pickle.load(f) 
+
+app = fa.Flask(__name__, template_folder='templates')
 
 @app.route('/')
 
+
 def main():
-    return(f.render_template('main.html'))
+    if fa.request.method == 'GET':
+        return(fa.render_template('main.html'))
+
+    if fa.request.method == 'POST':        
+        
+        satisfaction_level = fa.request.form['satisfaction_level']
+        last_evaluation = fa.request.form['last_evaluation']
+        number_of_projects = fa.request.form['number_of_projects']   
+        average_monthly_hours= fa.request.form['average_monthly_hours']   
+        years_at_company = fa.request.form['years_at_company']   
+        work_accident = fa.request.form['work_accident']   
+        promotion_last_5years = fa.request.form['promotion_last_5years']   
+        department = fa.request.form['department']   
+        salary = fa.request.form['salary']   
+        
+
+        input_variables = pd.DataFrame([[satisfaction_level, last_evaluation, number_of_projects,
+        
+                                        average_monthly_hours,years_at_company, work_accident,
+                                        promotion_last_5years, department, salary]],
+                                       columns=['satisfaction_level', 'last_evaluation',
+                                        'number_of_projects','average_monthly_hours',
+                                        'years_at_company','work_accident',
+                                        'promotion_last_5years', 'department', 'salary'],
+                                       dtype=float)        
+        prediction = model.predict(input_variables)[0]        
+        return f.render_template('main.html',
+                                  original_input={'satisfaction_level':satisfaction_level,
+                                                     'last_evaluation':last_evaluation,
+                                                     'number_of_projects':number_of_projects},
+                                     result=prediction,
+                                     )
 
 if __name__ == '__main__':
     app.run()
+
